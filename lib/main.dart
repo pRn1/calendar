88542b1792b2eed42695ba2ee8981d8ebb9921b3
Lucas <ruyplucs@gmail.com>
2022-07-20 19:38:04 -0300

    [feat] responsible pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
+import 'package:calendar_try/wrap_services_buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-import 'package:calendar_try/scaffold_pattern.dart';
-import 'package:calendar_try/toggle_buttons_schedule.dart';
-
 import 'container_pending.dart';
+import 'scaffold_pattern.dart';
 import 'top_container_pattern.dart';
 import 'utils.dart';
+import 'wrap_hour_buttons.dart';
 
 void main() {
   initializeDateFormatting().then(
@@ -33,12 +33,6 @@ class _CalendarPageState extends State<CalendarPage> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
 
-  int indexSelected = 0;
-  changeIndexSelected(int index) {
-    indexSelected = index;
-    setState(() {});
-  }
-
   @override
   void initState() {
     super.initState();
@@ -49,13 +43,12 @@ class _CalendarPageState extends State<CalendarPage> {
     if (!isSameDay(_selectedDay, selectedDay)) {
       setState(
         () {
+          daySelected = DateFormat.MEd('pt_BR').format(selectedDay);
           _selectedDay = selectedDay;
           _focusedDay = focusedDay;
-          day = DateFormat.MEd('pt_BR').format(selectedDay);
         },
       );
     }
-    day = DateFormat.MEd('pt_BR').format(selectedDay);
   }
 
   @override
@@ -143,38 +136,7 @@ class _CalendarPageState extends State<CalendarPage> {
                   ),
                 ],
               ),
-              // Wrap(
-              //   spacing: 10,
-              //   children: [
-              //     SizedBox(
-              //       width: 120,
-              //       child: MyButton(
-              //           text: 'Corte Máquina', index: ),
-              //     ),
-              //     SizedBox(
-              //       width: 120,
-              //       child: MyButton(text: 'Barba', index: ),
-              //     ),
-              //     SizedBox(
-              //       width: 120,
-              //       child: MyButton(text: '-', index: ),
-              //     ),
-              //     SizedBox(
-              //       width: 120,
-              //       child: MyButton(
-              //           text: 'Corte Tesoura', index: ),
-              //     ),
-              //     SizedBox(
-              //       width: 120,
-              //       child:
-              //           MyButton(text: 'Hidratação', index: ),
-              //     ),
-              //     SizedBox(
-              //       width: 120,
-              //       child: MyButton(text: '-', index: ),
-              //     ),
-              //   ],
-              // ),
+              const WrapServicesButtons(),
               const SizedBox(height: 10),
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
@@ -191,203 +153,7 @@ class _CalendarPageState extends State<CalendarPage> {
                   ),
                 ],
               ),
-              Wrap(
-                spacing: 10,
-                alignment: WrapAlignment.center,
-                children: [
-                  MyButton(
-                    text: '08:30',
-                    index: 2,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '08:30';
-
-                      changeIndexSelected(2);
-                    },
-                  ),
-                  MyButton(
-                    text: '09:00',
-                    index: 3,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '09:00';
-
-                      changeIndexSelected(3);
-                    },
-                  ),
-                  MyButton(
-                    text: '09:30',
-                    index: 4,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '09:30';
-                      changeIndexSelected(4);
-                    },
-                  ),
-                  MyButton(
-                    text: '10:00',
-                    index: 5,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '10:00';
-                      changeIndexSelected(5);
-                    },
-                  ),
-                  MyButton(
-                    text: '10:30',
-                    index: 6,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '10:30';
-                      changeIndexSelected(6);
-                    },
-                  ),
-                  MyButton(
-                    text: '11:00',
-                    index: 7,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '11:00';
-                      changeIndexSelected(7);
-                    },
-                  ),
-                  MyButton(
-                    text: '11:30',
-                    index: 8,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '11:30';
-                      changeIndexSelected(8);
-                    },
-                  ),
-                  MyButton(
-                    text: '12:00',
-                    index: 9,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '12:00';
-                      changeIndexSelected(9);
-                    },
-                  ),
-                  MyButton(
-                    text: '12:30',
-                    index: 10,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '12:30';
-                      changeIndexSelected(10);
-                    },
-                  ),
-                  MyButton(
-                    text: '13:00',
-                    index: 11,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '13:00';
-                      changeIndexSelected(11);
-                    },
-                  ),
-                  MyButton(
-                    text: '13:30',
-                    index: 12,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '13:30';
-                      changeIndexSelected(12);
-                    },
-                  ),
-                  MyButton(
-                    text: '14:00',
-                    index: 13,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '14:00';
-                      changeIndexSelected(13);
-                    },
-                  ),
-                  MyButton(
-                    text: '14:30',
-                    index: 14,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '14:30';
-                      changeIndexSelected(14);
-                    },
-                  ),
-                  MyButton(
-                    text: '15:00',
-                    index: 15,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '15:00';
-                      changeIndexSelected(15);
-                    },
-                  ),
-                  MyButton(
-                    text: '15:30',
-                    index: 16,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '15:30';
-                      changeIndexSelected(16);
-                    },
-                  ),
-                  MyButton(
-                    text: '16:00',
-                    index: 17,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '16:00';
-                      changeIndexSelected(17);
-                    },
-                  ),
-                  MyButton(
-                    text: '16:30',
-                    index: 18,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '16:30';
-                      changeIndexSelected(18);
-                    },
-                  ),
-                  MyButton(
-                    text: '17:00',
-                    index: 19,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '17:00';
-                      changeIndexSelected(19);
-                    },
-                  ),
-                  MyButton(
-                    text: '17:30',
-                    index: 20,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '17:30';
-                      changeIndexSelected(20);
-                    },
-                  ),
-                  MyButton(
-                    text: '18:00',
-                    index: 21,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '18:00';
-                      changeIndexSelected(21);
-                    },
-                  ),
-                  MyButton(
-                    text: '18:30',
-                    index: 22,
-                    indexSelected: indexSelected,
-                    onPressed: () {
-                      hour = '18:30';
-                      changeIndexSelected(22);
-                    },
-                  ),
-                ],
-              ),
+              const WrapHourButtons(),
               Padding(
                 padding: const EdgeInsets.only(right: 15),
                 child: Row(
@@ -399,51 +165,6 @@ class _CalendarPageState extends State<CalendarPage> {
               )
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyButton extends StatefulWidget {
-  const MyButton({
-    Key? key,
-    required this.index,
-    required this.indexSelected,
-    required this.text,
-    required this.onPressed,
-  }) : super(key: key);
-
-  @override
-  State<MyButton> createState() => _MyButtonState();
-
-  final int index;
-  final int indexSelected;
-  final String text;
-  final void Function() onPressed;
-}
-
-class _MyButtonState extends State<MyButton> {
-  bool isFavorite = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialButton(
-      // key: UniqueKey(),
-      color: widget.indexSelected == widget.index
-          ? Colors.black
-          : const Color.fromARGB(255, 66, 66, 66),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-        side: const BorderSide(color: Colors.white),
-      ),
-      onPressed: widget.onPressed,
-      child: Text(
-        widget.text,
-        style: TextStyle(
-          color: isFavorite ? Colors.grey : Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 13,
         ),
       ),
     );
@@ -472,7 +193,7 @@ class __ScheduleButtonState extends State<ScheduleButton> {
         Navigator.pushNamed(
           context,
           ExtractArgumentsScreen.routeName,
-          arguments: ScreenArguments(hour, day),
+          arguments: ScreenArguments(hourSelected, daySelected),
         );
       },
       child: Row(
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,12 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:table_calendar/table_calendar.dart';
-
-import 'container_pending.dart';
 
 final kToday = DateTime.now();
 final kFirstDay = DateTime(kToday.day);
 final kLastDay = DateTime(2022, 12, 31);
-String? hour;
-String? day;
-bool isPressed = false;
+String? hourSelected;
+String daySelected = DateFormat.MEd('pt_BR').format(kToday);
+bool isFavorite = false;
+
+class HoursButtons extends StatefulWidget {
+  const HoursButtons({
+    Key? key,
+    required this.index,
+    required this.indexSelected,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  State<HoursButtons> createState() => _HoursButtonsState();
+
+  final int index;
+  final int indexSelected;
+  final String text;
+  final void Function() onPressed;
+}
+
+class _HoursButtonsState extends State<HoursButtons> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      color: widget.indexSelected == widget.index
+          ? Colors.black
+          : const Color.fromARGB(255, 66, 66, 66),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+        side: const BorderSide(color: Colors.white),
+      ),
+      onPressed: widget.onPressed,
+      child: Text(
+        widget.text,
+        style: TextStyle(
+          color: isFavorite ? Colors.grey : Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 13,
+        ),
+      ),
+    );
+  }
+}
+
+class ServicesButtons extends StatefulWidget {
+  const ServicesButtons({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  State<ServicesButtons> createState() => _ServicesButtonsState();
+
+  final String text;
+}
+
+class _ServicesButtonsState extends State<ServicesButtons> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      // key: UniqueKey(),
+      color: isFavorite ? Colors.black : const Color.fromARGB(255, 66, 66, 66),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+        side: const BorderSide(color: Colors.white),
+      ),
+      onPressed: () {
+        setState(() {
+          isFavorite = !isFavorite;
+        });
+      },
+      child: Text(
+        widget.text,
+        style: TextStyle(
+          color: isFavorite ? Colors.grey : Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 13,
+        ),
+      ),
+    );
+  }
+}
